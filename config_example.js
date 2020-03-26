6eb881d38755cef2f97976856eb8353eb8f600f8
Dave Ferguson <dave@harmonydata.co.uk>
2020-03-26 11:10:21 +0000

    Initial creation

---
+++ config_example.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'aws': {
+        'key': '...',           // Enter key
+        'secret': '...',        // Enter secret
+        'ses': {
+            'from': {
+                'default': '"Sharemytutoring.com" <noreply@sharemytutoring.com>', // Replace with your email
+            },
+            'region': 'us-east-1' // Replace with the region your SES is in
+        }
+    }
+};
